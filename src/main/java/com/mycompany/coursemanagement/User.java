b6438c3953f545735094f91ad6153b8befddd18a
ShahdSaed <shahd01278039699@gmail.com>
2024-11-15 15:36:53 +0200

    Update User.java

--- src/main/java/com/mycompany/coursemanagement/User.java
+++ src/main/java/com/mycompany/coursemanagement/User.java
@@ -2,54 +2,45 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package com.mycompany.coursemanagement;
+package java_project;
 
-/**
- *
- * @author dell
- */
+import java.util.*;
 
-abstract class User {
+public abstract class User {
+    // Attributes
     private String name;
-    private String email;
     private String password;
-
-    public User(String name, String email, String password) {
+    private String email;
+    
+    //constructor
+    public User(String name, String password, String email) {
         this.name = name;
-        this.email = email;
         this.password = password;
+        this.email = email;
     }
-
+    
+    //setter and getters
     public String getName() {
         return name;
     }
-
     public void setName(String name) {
         this.name = name;
     }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
     public String getPassword() {
         return password;
     }
-
     public void setPassword(String password) {
         this.password = password;
     }
-
-    public void login() {
-        System.out.println(name + " logged in.");
+    public String getEmail() {
+        return email;
     }
-
-    public void logout() {
-        System.out.println(name + " logged out.");
+    public void setEmail(String email) {
+        this.email = email;
     }
+    
+    // check if the user has account in website or not 
+    public abstract void logIn(String email,String password);
+    
+    public abstract void logOut();
 }
-
